6cd3e628f6c9331c1277a94d19d8f03534dcf934
Marlon Bartoli <marlonbartoli@gmail.com>
2025-01-14 16:54:20 -0300

    Transformando em uma aplicação java web

---
+++ src/main/java/com/barbearia/agendaCorte/AgendaCorteApplication.java
@@ -0,0 +1,13 @@
+package com.barbearia.agendaCorte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaCorteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaCorteApplication.class, args);
+	}
+
+}
