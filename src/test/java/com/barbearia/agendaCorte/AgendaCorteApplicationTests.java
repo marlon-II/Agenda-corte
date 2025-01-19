66329363edf9e42401efbbba22594afa736bae2c
Marlon Bartoli <marlonbartoli@gmail.com>
2025-01-19 15:18:00 -0300

    Configurando para arquivo .war

---
+++ src/test/java/com/barbearia/agendaCorte/AgendaCorteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbearia.agendaCorte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaCorteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
